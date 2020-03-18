0b08523342d5d4febdc45750c97adf88bef0bf86
Dmitry Rukovichnikov <dmitry.rukovichnikov@gmail.com>
2020-03-18 15:43:12 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
 exports.min = function min (array) {
-  return 0;
+  
+  if (array == undefined){
+    return 0;
+    } else if (array.length == 0) {
+      return 0;
+    }
+  
+  let minValue = Math.min (...array);
+  return minValue;
+  
+  
+
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+
+  if (array == undefined){
+    return 0;
+    } else if (array.length == 0) {
+      return 0;
+    }
+  
+
+  let maxValue = Math.max (...array);
+  return maxValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+  if (array == undefined){
+    return 0;
+    } else if (array.length == 0) {
+      return 0;
+    }
+  
+
+  let sumValue = array.reduce (function (a,b) {
+    return (a + b);
+  })
+  let avgValue = sumValue/array.length
+  return avgValue;
 }
